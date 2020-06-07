71b3efc35073bde534b4f76473429722156eef3c
José Luís Soares Batista <zeluis.ap@gmail.com>
2020-06-07 17:33:42 -0300

    Swagger server

--- src/main.ts
+++ src/main.ts
@@ -4,6 +4,7 @@ import * as session from 'express-session';
 import { ValidationPipe } from '@nestjs/common';
 import { ConfigService } from '@nestjs/config';
 const MongoStore = require('connect-mongo')(session);
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
@@ -30,6 +31,17 @@ async function bootstrap() {
       whitelist: true,
     }),
   );
+
+  const options = new DocumentBuilder()
+    .setTitle('Dropship backend')
+    .setDescription('The dropship API description')
+    .setVersion('1.0')
+    .addTag('dropship')
+    .build();
+
+  const document = SwaggerModule.createDocument(app, options);
+
+  SwaggerModule.setup('api', app, document);
 
   await app.listen(configService.get('PORT') || 3000);
 }
